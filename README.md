ff9e83ae0eff8e24bd92fffdebaf99ff8cbce87e
Jeremy Stephens <viking@pillageandplunder.net>
2016-02-26 09:58:11 -0600

    Add repeat mode
    Novelty salt: <(Zhj

--- README.md
+++ README.md
@@ -14,18 +14,30 @@ Build Dependencies
 Syntax
 ------
 
-`git-novelty -m <message> [ -p <prefix> ]`
+`git-novelty -m <message> (-p <prefix>|-r <repeat> -c <cycle>)`
 
 If `git-novelty` is in your shell path, you can use:
 
-`git novelty -m <message> [ -p <prefix> ]`
+`git novelty -m <message> (-p <prefix>|-r <repeat> -c <cycle>)`
+
+Modes
+-----
+
+In prefix mode, git-novelty will try to create a commit with a hash that begins
+with your specified string. For example, `git-novelty -m "foo" -p "0123456"`
+might produce this hash: *0123456*9ba676004aa72a83ed2ce0f61ab663fc5.
+
+In repeat mode, git-novelty will try to create a commit with a hash that has a
+repeating pattern for each cycle you specify. For example, `git-novelty -m
+"foo" -r "0123" -c 10` might produce this hash:
+*0123*bf19ba*0123*04aa72*0123*d2ce0f*0123*663fc5.
 
 Caveats
 -------
 
 This works by adding in salt to the commit message, kind of like how Bitcoin
 hashing works. Finding the desired hash can take a very long time, depending on
-how long your prefix is.
+the number of target bytes.
 
 Example
 -------
--- main.go
+++ main.go
@@ -8,80 +8,156 @@ import (
 	"time"
 )
 
-var commitFormat string = "tree %s\nparent %s\nauthor %s <%s> %d %+05d\ncommitter %[3]s <%[4]s> %[5]d %+05[6]d\n\n%s\n\nNovelty salt: %x\n"
+const (
+	ModeNone = iota
+	ModePrefix
+	ModeRepeat
+)
+
+var commitFormat string = "tree %s\nparent %s\nauthor %s <%s> %d %+05d\ncommitter %[3]s <%[4]s> %[5]d %+05[6]d\n\n%s\n\nNovelty salt: %s\n"
+
+func hexStringToByteSlices(s string) (match []byte, mask []byte, err error) {
+	match = make([]byte, 20)
+	mask = make([]byte, 20)
+
+	var x byte
+	for i, c := range s {
+		switch c {
+		case '0':
+			x = 0
+		case '1':
+			x = 1
+		case '2':
+			x = 2
+		case '3':
+			x = 3
+		case '4':
+			x = 4
+		case '5':
+			x = 5
+		case '6':
+			x = 6
+		case '7':
+			x = 7
+		case '8':
+			x = 8
+		case '9':
+			x = 9
+		case 'a':
+			x = 10
+		case 'b':
+			x = 11
+		case 'c':
+			x = 12
+		case 'd':
+			x = 13
+		case 'e':
+			x = 14
+		case 'f':
+			x = 15
+		default:
+			err = fmt.Errorf("illegal character: %v", c)
+			break
+		}
+
+		if i%2 == 0 {
+			match[i/2] = x << 4
+			mask[i/2] = 0xf0
+		} else {
+			match[i/2] |= x
+			mask[i/2] |= 0x0f
+		}
+	}
+
+	return
+}
+
+var digits94 string = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+func base94(i int) string {
+	var b []byte
+	for j := i; j > 0; j /= 94 {
+		b = append([]byte{digits94[j%94]}, b...)
+	}
+	return string(b)
+}
 
 func main() {
 	var (
-		msg, strPrefix string
-		err            error
-		prefix, mask   []byte
+		msg         string
+		strPrefix   string
+		strRepeat   string
+		cycle       uint
+		err         error
+		match, mask []byte
+		mode        int
 	)
 
 	flag.StringVar(&msg, "m", "", "commit message")
-	flag.StringVar(&strPrefix, "p", "beef", "desired hash prefix")
+	flag.StringVar(&strPrefix, "p", "", "desired prefix")
+	flag.StringVar(&strRepeat, "r", "", "desired repeat")
+	flag.UintVar(&cycle, "c", 5, "cycle for repeating")
 	flag.Parse()
 
-	if msg == "" || strPrefix == "" {
-		err = fmt.Errorf("message and prefix are required")
-	} else if len(strPrefix) >= 40 {
-		err = fmt.Errorf("prefix is too long")
+	if msg == "" {
+		err = fmt.Errorf("message is required")
 	} else {
-		var x, y byte
-		for i, c := range strPrefix {
-			switch c {
-			case '0':
-				x = 0
-			case '1':
-				x = 1
-			case '2':
-				x = 2
-			case '3':
-				x = 3
-			case '4':
-				x = 4
-			case '5':
-				x = 5
-			case '6':
-				x = 6
-			case '7':
-				x = 7
-			case '8':
-				x = 8
-			case '9':
-				x = 9
-			case 'a':
-				x = 10
-			case 'b':
-				x = 11
-			case 'c':
-				x = 12
-			case 'd':
-				x = 13
-			case 'e':
-				x = 14
-			case 'f':
-				x = 15
-			default:
-				err = fmt.Errorf("illegal character: %v", c)
-				break
-			}
-
-			if i%2 == 0 {
-				y = x << 4
+		if strPrefix != "" {
+			if len(strPrefix) >= 40 {
+				err = fmt.Errorf("prefix is too long")
 			} else {
-				y |= x
-				prefix = append(prefix, y)
-				mask = append(mask, 0xff)
+				mode = ModePrefix
 			}
 		}
-		if len(strPrefix)%2 == 1 {
-			prefix = append(prefix, y)
-			mask = append(mask, 0xf0)
+		if err == nil && strRepeat != "" {
+			if mode != ModeNone {
+				err = fmt.Errorf("prefix and repeat settings are mutually exclusive")
+				mode = ModeNone
+			} else if cycle >= 40 || cycle == 0 {
+				err = fmt.Errorf("cycle must be between 1 and 40 (inclusive)")
+			} else {
+				mode = ModeRepeat
+			}
+		}
+		if err == nil && mode == ModeNone {
+			err = fmt.Errorf("prefix or repeat is required")
+		}
+	}
+
+	switch mode {
+	case ModePrefix:
+		match, mask, err = hexStringToByteSlices(strPrefix)
+	case ModeRepeat:
+		match, mask, err = hexStringToByteSlices(strRepeat)
+
+		if err == nil {
+			for i, j := cycle, 0; i < 40; i++ {
+				if i%cycle == 0 {
+					j = 0
+				}
+				if j < len(strRepeat) {
+					var c byte
+					if j%2 == 0 {
+						c = match[j/2] >> 4
+					} else {
+						c = match[j/2] & 0x0f
+					}
+
+					if i%2 == 0 {
+						match[i/2] = c<<4 + match[i/2]&0x0f
+						mask[i/2] |= 0xf0
+					} else {
+						match[i/2] = c + match[i/2]&0xf0
+						mask[i/2] |= 0x0f
+					}
+					j++
+				}
+			}
 		}
 	}
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\nUsage: %s -m <message> [-p <prefix>]\n", err, os.Args[0])
+		fmt.Fprintf(os.Stderr, "Error: %s\nUsage: %s -m <message> (-p <prefix>|-r <repeat> -c <cycle>)\n", err, os.Args[0])
 		return
 	}
 
@@ -159,7 +235,7 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Attempt:", i+1)
 		}
 
-		content = fmt.Sprintf(commitFormat, tree, head.Target(), user, email, now.Unix(), offset/36, msg, i)
+		content = fmt.Sprintf(commitFormat, tree, head.Target(), user, email, now.Unix(), offset/36, msg, base94(i))
 		oid, err = odb.Hash([]byte(content), git.ObjectCommit)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error hashing object:", err)
@@ -167,8 +243,8 @@ func main() {
 		}
 
 		ok = true
-		for i, p := range prefix {
-			if oid[i]&mask[i] != p {
+		for i, c := range oid {
+			if c&mask[i] != match[i] {
 				ok = false
 				break
 			}
